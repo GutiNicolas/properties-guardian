83cb7833257c5e3205c8aa174ed40485d8805684
GutiNicolas <luis.gutierrez@despegar.com>
2020-04-28 19:26:38 -0300

    First Commit

---
+++ src/main/java/com/gutinicolas/propertiesguardian/PropertiesGuardian.java
@@ -0,0 +1,13 @@
+package com.gutinicolas.propertiesguardian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertiesGuardian {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertiesGuardian.class, args);
+	}
+
+}
---
+++ src/test/java/com/gutinicolas/propertiesguardian/PropertiesGuardianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gutinicolas.propertiesguardian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertiesGuardianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
